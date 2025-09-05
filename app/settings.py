3c7c3171453973a60a4bdb26f79aa5e339035c4e
Dario <dario@mail.com>
2025-09-05 23:53:03 +0200

    Update static file url

--- app/settings.py
+++ app/settings.py
@@ -128,14 +128,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
+STATIC_URL = '/static/'
 if DEBUG:
-    STATIC_URL = 'static/'
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static'),
     ]
 else:
-    STATIC_URL = 'staticfiles/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
