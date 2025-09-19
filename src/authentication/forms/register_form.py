86b92cd818fa1b4bd868925a3b4d79b928e18b7e
Dario <dario@mail.com>
2025-09-20 00:18:31 +0200

    Move after registration logic to service

--- src/authentication/forms/register_form.py
+++ src/authentication/forms/register_form.py
@@ -1,10 +1,9 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import Group
 from django.core.validators import RegexValidator
 
-from src.user.enum import UserEnum
-from src.user.models import User as User, UserProfile
+from src.authentication.services.post_registration_service import PostRegistrationService
+from src.user.models import User as User
 
 alphanumeric = RegexValidator(
     r'^[0-9a-zA-Z]*$',
@@ -15,19 +14,14 @@ alphanumeric = RegexValidator(
 class RegisterForm(UserCreationForm):
     username = forms.CharField(max_length=50, required=True, validators=[alphanumeric])
     email = forms.EmailField()
-    role = forms.ChoiceField(choices=UserEnum.roles(), required=True)
     accept_tos = forms.BooleanField(required=True, label='I accept Terms Of Use')
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2', 'role', 'accept_tos']
+        fields = ['username', 'email', 'password1', 'password2', 'accept_tos']
 
     def save(self, commit=True):
+        post_registration_service = PostRegistrationService()
         user = super().save(commit)
-        role = self.cleaned_data['role']
-        if role == UserEnum.ROLE_ADMIN.value:
-            role = UserEnum.ROLE_USER.value
-        Group.objects.get_or_create(name=role)
-        user.groups.add(Group.objects.get(name=role))
-        UserProfile.objects.create(user=user)
+        post_registration_service.register(user)
         return user
--- src/authentication/signals.py
+++ src/authentication/signals.py
@@ -1,12 +1,10 @@
 from allauth.account.signals import user_signed_up
 from django.dispatch import receiver
 
-from src.user.models import UserProfile
+from src.authentication.services.post_registration_service import PostRegistrationService
 
 
 @receiver(user_signed_up)
 def after_signup(request, user, **kwargs):
-    try:
-        profile = user.profile
-    except UserProfile.DoesNotExist:
-        UserProfile.objects.create(user=user)
+    service = PostRegistrationService()
+    service.register(user)
