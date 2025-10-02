a5d01af90f82a944223ea9e5e8adc84a44b12ac1
Dario <dario@mail.com>
2025-10-02 22:24:34 +0200

    When there is no content display

--- src/media/services/my_content/my_content_service.py
+++ src/media/services/my_content/my_content_service.py
@@ -9,8 +9,12 @@ class MyContentService:
     PER_PAGE = 25
 
     def list_my_content(self, user: User, current_page: int) -> Page[Media]:
-        media = Media.objects.filter(user=user).exclude(status=MediaEnum.STATUS_DELETED.value).order_by('-id')
+        media = (Media.objects
+                 .filter(user=user)
+                 .filter(id=6666)
+                 .exclude(status=MediaEnum.STATUS_DELETED.value)
+                 .order_by('-id'))
         paginator = Paginator(object_list=media, per_page=self.PER_PAGE)
-        page = paginator.page(current_page)
+        page: Page = paginator.page(current_page)
 
         return page
