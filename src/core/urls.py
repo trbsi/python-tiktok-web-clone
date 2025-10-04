7ba63abe6f0bb370958e119d1ec58bb003b45b6d
Dario <dario@mail.com>
2025-10-04 13:44:41 +0200

    Fix trailer path

--- src/core/urls.py
+++ src/core/urls.py
@@ -4,7 +4,8 @@ from src.feed import views as feed_views
 from . import views
 
 urlpatterns = [
-    path('', feed_views.discover, name='home'),
+    # TODO redirect based on if user is authenticated or not
+    path('', feed_views.following, name='home'),
     path('terms-of-use', views.terms_of_use, name='terms_of_use'),
     path('privacy-policy', views.privacy_policy, name='privacy_policy'),
     path('content-moderation-policy', views.content_moderation_policy, name='content_moderation_policy'),
