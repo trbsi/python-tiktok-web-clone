75347cb36831910746c82d828909fa2c44031533
Dario <dario@mail.com>
2025-10-07 19:38:10 +0200

    Add balance endpoint

--- src/payment/urls.py
+++ src/payment/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('my-payments', views.my_payments, name='payment.my_payments'),
+    path('api/balance', views.api_get_balance, name='payment.api.get_balance'),
 ]
--- src/payment/views.py
+++ src/payment/views.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpRequest, HttpResponse
+from django.contrib.auth.models import AnonymousUser
+from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views.decorators.http import require_GET
 
+from src.payment.models import Balance
 from src.payment.services.my_payments.my_payments_service import MyPaymentsService
+from src.user.models import User
 
 
 @require_GET
@@ -26,3 +29,15 @@ def my_payments(request: HttpRequest) -> HttpResponse:
             'user': user
         },
     )
+
+
+@require_GET
+@login_required
+def api_get_balance(request: HttpRequest) -> JsonResponse:
+    user: User | AnonymousUser = request.user
+    if user.is_anonymous:
+        return JsonResponse({"balance": None})
+
+    balance: Balance = Balance.objects.get(user=user)
+
+    return JsonResponse({"balance": balance.balance})
