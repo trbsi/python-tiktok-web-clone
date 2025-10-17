ddea098d5075cc278f3cb4ac277ff21d0f2a0b75
Dario <dario@mail.com>
2025-10-17 22:06:57 +0200

    use Enum values

--- src/media/services/upload_media/upload_media_service.py
+++ src/media/services/upload_media/upload_media_service.py
@@ -41,11 +41,11 @@ class UploadMediaService:
 
         match post_type:
             case 'post_now':
-                status = MediaEnum.STATUS_PAID
+                status = MediaEnum.STATUS_PAID.value
             case 'schedule':
-                status = MediaEnum.STATUS_SCHEDULE
+                status = MediaEnum.STATUS_SCHEDULE.value
             case _:
-                status = MediaEnum.STATUS_PAID
+                status = MediaEnum.STATUS_PAID.value
 
         media = Media.objects.create(
             file_info=remote_file_info,
