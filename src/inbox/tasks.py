97386f7671d72469fea7adbabebf7bc536232073
Dario <dario@mail.com>
2025-11-03 19:25:58 +0100

    add task name

--- src/inbox/tasks.py
+++ src/inbox/tasks.py
@@ -3,7 +3,7 @@ from celery import shared_task
 from src.inbox.crons.auto_reply.auto_reply_task import AutoReplyTask
 
 
-@shared_task
+@shared_task(name='task_auto_reply')
 def task_auto_reply(message_id: int):
     task = AutoReplyTask()
     task.auto_reply(message_id)
