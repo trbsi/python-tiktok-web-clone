faedebd53b7395bfd51ed6cd9ba92b796ce0ec4e
Dario <dario@mail.com>
2025-11-03 17:41:44 +0100

    Fix payment flow

--- src/payment/services/buy_package/buy_package_service.py
+++ src/payment/services/buy_package/buy_package_service.py
@@ -1,3 +1,5 @@
+import random
+
 from app import settings
 from src.payment.enums import PaymentEnum
 from src.payment.models import PaymentHistory, Package
@@ -14,20 +16,27 @@ class BuyPackageService():
     def buy_package(self, user: User, package_id) -> str:
         package = Package.objects.get(id=package_id)
 
+        # @TODO remove this variable
+        random_int = random.randint(1, 10000)
+        random_int = f'default_no_id_{random_int}'
+
         payment_history = PaymentHistory.objects.create(
             user=user,
             price=package.price,
             amount=package.amount,
             provider=settings.DEFAULT_PAYMENT_PROVIDER,
-            provider_payment_id='default_no_id',
+            provider_payment_id=random_int,
             status=PaymentEnum.STATUS_PENDING.value,
         )
-
-        # @TODO remove payment_webhook call, this is just for debug
-        PaymentWebhookService().handle_webook({})
 
         value_object: CheckoutValueObject = self.payment_provider_service.create_checkout(payment_history)
         payment_history.provider_payment_id = value_object.provider_payment_id
         payment_history.save()
+
+        # @TODO remove payment_webhook call, this is just for debug
+        PaymentWebhookService().handle_webook({
+            'X-paymentid': random_int,
+            'eventType': 'NewSaleSuccess',
+        })
 
         return value_object.redirect_url
