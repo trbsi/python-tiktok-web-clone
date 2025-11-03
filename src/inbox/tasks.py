2ec9283e42c418280ad96b62f74f42af47b6ecf8
Dario <dario@mail.com>
2025-11-03 19:36:57 +0100

    Fix celery worker

--- src/inbox/tasks.py
+++ src/inbox/tasks.py
@@ -3,7 +3,7 @@ from celery import shared_task
 from src.inbox.crons.auto_reply.auto_reply_task import AutoReplyTask
 
 
-@shared_task(name='task_auto_reply')
+@shared_task()
 def task_auto_reply(message_id: int):
     task = AutoReplyTask()
     task.auto_reply(message_id)
