3412d6b2f34a752e4a64fff3d073c860cccad1cb
Dario <dario@mail.com>
2025-11-04 17:25:15 +0100

    Print raw response

--- src/inbox/crons/auto_reply/auto_reply_task.py
+++ src/inbox/crons/auto_reply/auto_reply_task.py
@@ -73,10 +73,10 @@ class AutoReplyTask:
             else:
                 chat_history.append({'role': role, 'content': message.message})
 
-        print(last_messages)
         print(chat_history)
         # Get reply and save
         reply = self._get_reply_from_ai(chat_history)
+        print(replies)
         replies = self._split_sentences_randomly(reply)
 
         for reply in replies:
