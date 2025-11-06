4531330a28e4e9b21d82f58ddca2b91296dbb917
Dario <dario@mail.com>
2025-11-06 19:36:56 +0100

    Hide upload button if files are being uploaded

--- static/js/file_uploader.js
+++ static/js/file_uploader.js
@@ -2,6 +2,7 @@ function fileUploader(uploadApi) {
     return {
         files: [],           // all files in the UI (including uploaded)
         uploadedFiles: [],   // track files already uploaded
+        isUploading: false,
 
         handleFiles(event) {
             const selectedFiles = Array.from(event.target.files);
@@ -62,6 +63,17 @@ function fileUploader(uploadApi) {
                     if (!this.uploadedFiles.includes(fileData)) {
                         this.uploadedFiles.push(fileData);
                     }
+
+                    allUploaded = true
+                    this.files.forEach(file => {
+                        if (file.status !== 'completed') {
+                            allUploaded = false;
+                            break;
+                        }
+                    });
+                    if (allUploaded) {
+                        this.isUploading = false;
+                    }
                 } else {
                     fileData.statusMessage = "Upload failed ❌";
                     fileData.status = 'failed';
@@ -77,6 +89,7 @@ function fileUploader(uploadApi) {
         },
 
         uploadAllFiles(postType) {
+            this.isUploading = true;
             this.files.forEach(file => {
                 if (file.status !== 'completed') {
                     this.uploadFile(file, postType);
