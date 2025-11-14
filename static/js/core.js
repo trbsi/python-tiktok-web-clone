f0a04efa46dae1144a546d286bbcfd513d0b880d
Dario <dario@mail.com>
2025-11-14 15:36:13 +0100

    Add install app logic

--- static/js/core.js
+++ static/js/core.js
@@ -1,12 +1,44 @@
-document.addEventListener('alpine:init', () => {
-    Alpine.magic('utils', () => ({
-        getCsrfToken() {
-            return document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-        },
-    }))
-});
-
 function getCsrfToken() {
     return document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 }
 
+/* ------------- PWA site - install website on phone -------------  */
+if ("serviceWorker" in navigator) {
+    //serviceWorkerFile is defined in head.html
+    navigator.serviceWorker.register(serviceWorkerFile);
+}
+
+let deferredPrompt;
+
+window.addEventListener("beforeinstallprompt", (e) => {
+    e.preventDefault(); // prevent default mini-infobar
+    deferredPrompt = e;
+
+    // Show your custom install button
+    $("#installBanner").removeClass("hidden");
+});
+
+// iOS does not fire beforeinstallprompt and does not allow programmatic prompts.
+const isIOS = /iphone|ipad|ipod/i.test(window.navigator.userAgent);
+const isInStandalone = ("standalone" in window.navigator) && window.navigator.standalone;
+
+if (isIOS && !isInStandalone) {
+    $("#iosInstallHint").removeClass("hidden");
+}
+
+
+$("#installBtn").on("click", async () => {
+    if (!deferredPrompt) return;
+
+    deferredPrompt.prompt();
+    const choice = await deferredPrompt.userChoice;
+
+    if (choice.outcome === "accepted") {
+        console.log("User installed the app");
+    } else {
+        console.log("User dismissed the install prompt");
+    }
+
+    deferredPrompt = null;
+    $("#installBanner").addClass("hidden");
+});
